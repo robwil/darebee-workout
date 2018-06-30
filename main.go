0aa2e87cadd7ef80b7b9da9cb7f1d1b0f49fcf28
Rob Williams <rob.williams@bounceexchange.com>
2018-06-30 10:39:33 -0400

    basic functionality working

--- main.go
+++ main.go
@@ -4,20 +4,145 @@ import (
     "github.com/valyala/fasthttp"
     "log"
     "github.com/qiangxue/fasthttp-routing"
+    "io"
+    "cloud.google.com/go/vision/apiv1"
+    "golang.org/x/net/context"
+    "strings"
     "fmt"
+    "regexp"
+    "strconv"
+    "net/http"
+    "io/ioutil"
 )
 
 // TODO: eventually make this interactive with https://github.com/kataras/iris/#learn
 
-func Hello(ctx *routing.Context) error {
+func detectText(w io.Writer, file string) (string, error) {
+    ctx := context.Background()
+    client, err := vision.NewImageAnnotatorClient(ctx)
+    if err != nil {
+        return "", err
+    }
+    image := vision.NewImageFromURI(file)
+    annotations, err := client.DetectDocumentText(ctx, image, nil)
+    if err != nil {
+        return "", err
+    }
+    return annotations.Text, nil
+}
+
+var exceptions = map[string]string{
+    "alt-arm-leg-raises": "arm-leg-raises",
+    "lunges-exercise": "forward-lunges",
+}
+
+func getVideoName(line string) string {
+    line = strings.ToLower(line)
+    // extract names only when prefaced with exercise count
+    r := regexp.MustCompile("^(\\d+)\\s+(.+)")
+    matches := r.FindStringSubmatch(line)
+    if len(matches) >= 3 {
+        // handle "between sets" instruction; not an exercise so skip it
+        if strings.Contains(line, "between") {
+            return ""
+        }
+        // replace non-word chars with hyphen
+        r = regexp.MustCompile("[^\\w]")
+        str := r.ReplaceAllString(matches[2], "-")
+        // convert any multi hyphen to hyphen (making less sensitive to Google Vision mistakes)
+        r = regexp.MustCompile("-+")
+        str = r.ReplaceAllString(str, "-")
+        // for single word exercises, they append "-exercise" to it
+        if !strings.Contains(str, "-") {
+            str = str + "-exercise"
+        }
+        // check for any exceptional cases
+        if exceptions[str] != "" {
+            return exceptions[str]
+        }
+        return str
+    }
+    return ""
+}
+
+// TODO: I can probably make a simple <Select> box of workouts, which map to their workout names
+
+func getImageUrl(workout string, day string) (string, error) {
+    workout = strings.ToLower(workout)
+    dayNum, err := strconv.Atoi(day)
+    if err != nil {
+        return "", err
+    }
+    return fmt.Sprintf("https://darebee.com/images/programs/%s/web/day%02d.jpg", workout, dayNum), nil
+}
+
+func getVideoUrl(name string) string {
+    return fmt.Sprintf("https://darebee.com/exercises/%s.html", name)
+}
+
+func getYoutubeEmbed(videoUrl string) (string, error) {
+    resp, err := http.Get(videoUrl)
+    if err != nil {
+        return "", err
+    }
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return "", err
+    }
+    r := regexp.MustCompile("youtube\\.com/embed/([^?]+)\\?")
+    matches := r.FindStringSubmatch(string(body))
+    if len(matches) >= 2 {
+        return matches[1], nil
+    }
+    return "", nil
+}
+
+func PrintVideos(ctx *routing.Context) error {
     log.Printf("GET %s", ctx.RequestURI())
-    fmt.Fprintf(ctx, "Hello world")
+    workout := ctx.Param("workout")
+    day := ctx.Param("day")
+    imageUrl, err := getImageUrl(workout, day)
+    fmt.Fprintf(ctx, `<img src="%s" /><br/>`, imageUrl)
+    if err != nil {
+        return err
+    }
+    text, err :=  detectText(ctx, imageUrl)
+    if err != nil {
+        return err
+    }
+    lines := strings.Split(text, "\n")
+    for _, line := range lines {
+        videoName := getVideoName(line)
+        if videoName == "" {
+            continue
+        }
+        url := getVideoUrl(videoName)
+        embedUrl, err := getYoutubeEmbed(url)
+        if err != nil {
+            return err
+        }
+        if embedUrl != "" {
+            fmt.Fprintf(ctx,`
+                <h2>%s</h2>
+                <p>
+                    <a href="%s" target="_blank">%s</a> <br/>
+                    <iframe width="845" height="480" src="//www.youtube.com/embed/%s?rel=0&showinfo=0" frameborder="0" allowfullscreen></iframe>
+                </p>`, line, url, url, embedUrl)
+        } else {
+            fmt.Fprintf(ctx, `
+                <h2>%s</h2>
+                <p>Video not found</p>
+            `, line)
+        }
+    }
+    ctx.Response.Header.Set("Content-Type", "text/html")
     return nil
 }
 
 func main() {
     router := routing.New()
-    router.Get("/", Hello)
+    router.Get("/<workout>/<day>", PrintVideos)
     if err := fasthttp.ListenAndServe("127.0.0.1:5000", router.HandleRequest); err != nil {
         log.Fatalf("error in ListenAndServe: %s", err)
     }
---
+++ main_test.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+    "testing"
+    "gotest.tools/assert"
+)
+
+func TestGetVideoName(t *testing.T) {
+    t.Run("basic case", func(t *testing.T) {
+        assert.Equal(t, "knee-strikes", getVideoName("20 knee strikes"))
+        assert.Equal(t, "low-front-kicks", getVideoName("20 low front kicks"))
+        assert.Equal(t, "overhead-punches", getVideoName("20 overhead punches"))
+    })
+    t.Run("handle extra space", func(t *testing.T) {
+        assert.Equal(t, "knee-strikes", getVideoName("20  knee  strikes"))
+    })
+    t.Run("handle pluses", func(t *testing.T) {
+        assert.Equal(t, "jab-jab-cross", getVideoName("20 jab + jab + cross"))
+    })
+    t.Run("things that should be ignored", func(t *testing.T) {
+        assert.Equal(t, "", getVideoName("Foundation"))
+        assert.Equal(t, "", getVideoName("Day 3 Fighter"))
+        assert.Equal(t, "", getVideoName("Levell 3 sets"))
+        assert.Equal(t, "", getVideoName("Level II 5 sets"))
+        assert.Equal(t, "", getVideoName("Level III 7 sets"))
+        assert.Equal(t, "", getVideoName("o darebee.com"))
+        assert.Equal(t, "", getVideoName("2 minutes rest between sets"))
+    })
+    t.Run("case insensitivity", func(t *testing.T) {
+        assert.Equal(t, "knee-strikes", getVideoName("20 Knee Strikes"))
+        assert.Equal(t, "overhead-punches", getVideoName("20 OVerhead PunchES"))
+        assert.Equal(t, "", getVideoName("2 minutes REST beTweeN sets"))
+    })
+    t.Run("single word exercises", func(t *testing.T) {
+        assert.Equal(t, "bridges-exercise", getVideoName("10 bridges"))
+        assert.Equal(t, "skiers-exercise", getVideoName("20 skiers"))
+    })
+    t.Run("exceptions", func(t *testing.T) {
+        assert.Equal(t, "arm-leg-raises", getVideoName("10 alt arm / leg raises"))
+        assert.Equal(t, "forward-lunges", getVideoName("20 lunges"))
+    })
+}
+
+func TestGetImageUrl(t *testing.T) {
+    t.Run("basic case", func(t *testing.T) {
+        imageUrl, err := getImageUrl("foundation", "23")
+        assert.NilError(t, err)
+        assert.Equal(t, "https://darebee.com/images/programs/foundation/web/day23.jpg", imageUrl)
+    })
+    t.Run("pad zeroes", func(t *testing.T) {
+        imageUrl, err := getImageUrl("foundation", "3")
+        assert.NilError(t, err)
+        assert.Equal(t, "https://darebee.com/images/programs/foundation/web/day03.jpg", imageUrl)
+    })
+    t.Run("case insensitive", func(t *testing.T) {
+        imageUrl, err := getImageUrl("Foundation", "3")
+        assert.NilError(t, err)
+        assert.Equal(t, "https://darebee.com/images/programs/foundation/web/day03.jpg", imageUrl)
+    })
+    t.Run("number parsing", func(t *testing.T) {
+        imageUrl, err := getImageUrl("foundation", "004")
+        assert.NilError(t, err)
+        assert.Equal(t, "https://darebee.com/images/programs/foundation/web/day04.jpg", imageUrl)
+    })
+}
+
+// TODO: refactor this function so the test doesn't make actual HTTP request
+func TestGetYoutubeEmbed(t *testing.T) {
+    embedUrl, err := getYoutubeEmbed("https://darebee.com/exercises/burpees-with-push-up.html")
+    assert.NilError(t, err)
+    assert.Equal(t, "ZQzikdjmkKg", embedUrl)
+}
